d889463d130fa32de80cfaae620ddaa756dab588
Jose Ramirez <josecrmrz@gmail.com>
2018-02-01 20:28:50 -0600

    Simply decrement method

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -36,7 +36,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void decrement(View view) {
-        quantity--;
+        if (quantity > 0) {
+            quantity--;
+        }
 
         display(quantity);
     }
@@ -52,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
 
@@ -60,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        TextView priceTextView = findViewById(R.id.price_text_view);
+        priceTextView.setText(String.format("Total: %1$s\nThank you!", NumberFormat.getCurrencyInstance().format(number)));
     }
 }
